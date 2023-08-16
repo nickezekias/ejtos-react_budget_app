d70283969d8732c845138900a89eb87ace4d4521
Nick Ezekiel <nickezekias@gmail.com>
2023-08-16 07:09:32 -0400

    fixed budget min max vals

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,10 +1,34 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { budget, dispatch, expenses } = useContext(AppContext);
+    const totalExpenses = expenses.reduce((total, item) => {
+        return (total += item.cost);
+    }, 0);
+    const MAX_BUDGET = 20000;
+    const onBudgetChange = (val) => {
+        if (val < totalExpenses) {
+            alert("You cannot reduce the budget value lower than the spending")
+        } else if (val > MAX_BUDGET) {
+            alert("Your budget value cannot exceed £20,000")
+            dispatch({ type: "SET_BUDGET", payload: MAX_BUDGET })
+        } else {
+            dispatch({ type: "SET_BUDGET", payload: val })
+        }
+    }
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <span>Budget: £</span>
+            <input
+                required='required'
+                type='number'
+                id='budget'
+                step='10'
+                value={budget}
+                style={{ marginLeft: 0, size: 10 }}
+                onChange={(event) => onBudgetChange(event.target.value)}
+            >
+            </input>
         </div>
     );
 };
